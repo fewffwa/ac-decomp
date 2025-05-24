85511beea072465cd761d4e8493d9882e6c489ee
Jake Kallos <yourface255@yahoo.com>
2025-05-23 19:54:48 -0700

    Match eSK_init and eSK_mv in ef_shock

--- src/effect/ef_shock.c
+++ src/effect/ef_shock.c
@@ -1,4 +1,5 @@
 #include "ef_effect_control.h"
+#include "m_common_data.h"
 
 static void eSK_init(xyz_t pos, int prio, s16 angle, GAME* game, u16 item_name, s16 arg0, s16 arg1);
 static void eSK_ct(eEC_Effect_c* effect, GAME* game, void* ct_arg);
@@ -17,8 +18,33 @@ eEC_PROFILE_c iam_ef_shock = {
     // clang-format on
 };
 
+typedef struct Unk {
+    f32 unk_00;
+    f32 unk_04;
+    f32 unk_08;
+    f32 unk_0C;
+    f32 unk_10;
+    f32 unk_14;
+    f32 unk_18;
+} Unk;
+
 static void eSK_init(xyz_t pos, int prio, s16 angle, GAME* game, u16 item_name, s16 arg0, s16 arg1) {
-    // TODO
+    Unk local_28;
+    xyz_t local_34;
+    
+    local_28.unk_00 = 0.0f;
+    local_28.unk_04 = 1.0f;
+    local_28.unk_08 = 0.0f;
+    local_28.unk_0C = 0.0f;
+    local_28.unk_10 = -0.1f;
+    local_28.unk_14 = 0.0f;
+    local_28.unk_18 = 1.0f;
+    
+    local_34.x = 0.0;
+    local_34.y = 4.0;
+    local_34.z = 6.0;
+    
+    eEC_CLIP->make_effect_proc(0, pos, &local_34, game, &local_28, item_name, prio, 0,0) ;
 }
 
 static void eSK_ct(eEC_Effect_c* effect, GAME* game, void* ct_arg) {
@@ -26,7 +52,11 @@ static void eSK_ct(eEC_Effect_c* effect, GAME* game, void* ct_arg) {
 }
 
 static void eSK_mv(eEC_Effect_c* effect, GAME* game) {
-    // TODO
+    short temp = 14 - effect->timer;
+
+    if (temp == 0) {
+        sAdo_OngenTrgStart(45,&effect->position);
+}
 }
 
 static void eSK_dw(eEC_Effect_c* effect, GAME* game) {
